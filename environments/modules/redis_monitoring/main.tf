ba495f32a2d428c4cee67bb617f9c82f34010dbe
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2025-07-09 20:29:11 -0500

    Update main.tf

--- environments/modules/redis_monitoring/main.tf
+++ environments/modules/redis_monitoring/main.tf
@@ -1,5 +1,5 @@
 
-
+# --------------custom --------------
 resource "google_logging_metric" "oom_errors" {
   name   = "oom_errors"
   filter = "resource.type=\"redis_instance\" AND textPayload:\"OOM command not allowed\""
@@ -12,29 +12,48 @@ resource "google_logging_metric" "oom_errors" {
 }
 
 
-resource "google_monitoring_alert_policy" "evictions" {
-  display_name = "${var.environment} - Redis Evictions > 0"
+
+
+resource "google_monitoring_alert_policy" "test_eviction2" {
+  display_name = "test-eviction"
   combiner     = "OR"
+  enabled      = true
+
+  alert_strategy {
+    auto_close           = "21600s"
+    notification_prompts = ["OPENED", "CLOSED"]
+  }
+
+  notification_channels = [
+    "projects/intense-reason-458522-h4/notificationChannels/16837620454532209889"
+  ]
+  user_labels = {
+    project_id    = var.project_id
+    context       = "redis"
+    instance_id   = var.redis_instance_id
+    resource_type = "redis_instance"
+    region        = var.region
+  }
   conditions {
-    display_name = "Evictions > 0"
+    display_name = "Cloud Memorystore Redis Instance - Evicted Keys"
+
     condition_threshold {
-      filter          = "metric.type=\"redis.googleapis.com/stats/evicted_keys\" resource.type=\"redis_instance\" resource.label.\"instance_id\"=\"${var.redis_instance_id}\""
+      filter          = "resource.type = \"redis_instance\" AND metric.type = \"redis.googleapis.com/stats/evicted_keys\""
       comparison      = "COMPARISON_GT"
-      threshold_value = 0
-      duration        = "60s"
+      duration        = "0s"
+      threshold_value = 1
+
       trigger {
         count = 1
       }
+
       aggregations {
-        alignment_period     = "60s"
-        per_series_aligner   = "ALIGN_RATE"
-        cross_series_reducer = "REDUCE_NONE"
+        alignment_period   = "300s"
+        per_series_aligner = "ALIGN_RATE"
       }
     }
   }
-  notification_channels = [google_monitoring_notification_channel.email.id]
 }
-
 
 resource "google_monitoring_alert_policy" "oom_error_alert" {
   display_name = "${var.environment} - Redis OOM Error"
@@ -51,6 +70,13 @@ resource "google_monitoring_alert_policy" "oom_error_alert" {
       }
     }
   }
+  user_labels = {
+    project_id    = var.project_id
+    context       = "redis"
+    instance_id   = var.redis_instance_id
+    resource_type = "redis_instance"
+    region        = var.region
+  }
   notification_channels = [google_monitoring_notification_channel.email.id]
 }
 
@@ -63,24 +89,9 @@ resource "google_monitoring_notification_channel" "email" {
   }
 }
 
-resource "google_logging_metric" "redis_connection_lost" {
-  name        = "redis_replica_connection_lost"
-  description = "Counts how often Redis replica loses connection to the master"
 
-  filter = <<EOT
-resource.type="redis_instance"
-jsonPayload.message:"Connection with master lost"
-EOT
 
-  metric_descriptor {
-    metric_kind = "DELTA"
-    value_type  = "INT64"
-    unit        = "1"
-  }
-
-  label_extractors = {}
-}
-
+# --------------from gcp --------------
 resource "google_monitoring_alert_policy" "redis_memory_utilization" {
   display_name = "Cloud Redis - System Memory Utilization for dev-redis-instance(us-central1)"
 
@@ -197,23 +208,21 @@ resource "google_monitoring_alert_policy" "redis_failover" {
   }
 
   conditions {
-    display_name = "Cloud Memorystore Redis Instance - Node Role"
+    display_name = "Cloud Memorystore Redis Instance - Failover"
 
     condition_threshold {
-      filter = "resource.type = \"redis_instance\" AND resource.labels.instance_id = \"projects/${var.project_id}/locations/us-central1/instances/${var.redis_instance_id}\" AND metric.type = \"redis.googleapis.com/replication/role\""
-
-
+      filter          = "resource.type = \"redis_instance\" AND resource.labels.instance_id = \"projects/${var.project_id}/locations/${var.region}/instances/${var.redis_instance_id}\" AND metric.type = \"redis.googleapis.com/replication/role\""
       duration        = "0s"
       comparison      = "COMPARISON_GT"
       threshold_value = 0
 
-      trigger {
-        count = 1
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_COUNT"
       }
 
-      aggregations {
-        alignment_period   = "300s"
-        per_series_aligner = "ALIGN_STDDEV"
+      trigger {
+        count = 1
       }
     }
   }
@@ -222,10 +231,76 @@ resource "google_monitoring_alert_policy" "redis_failover" {
     auto_close = "604800s"
   }
 
-  notification_channels = [google_monitoring_notification_channel.email.id]
+  notification_channels = [
+    "projects/${var.project_id}/notificationChannels/16837620454532209889"
+  ]
 }
 
+# resource "google_monitoring_alert_policy" "redis_failover" {
+#   display_name = "Cloud Redis - Standard Instance Failover for dev-redis-instance(us-central1)"
 
+#   documentation {
+#     content   = "This alert fires if failover occurs for a standard tier instance. To ensure that alerts always fire, we recommend keeping the threshold value to 0"
+#     mime_type = "text/markdown"
+#   }
+
+#   combiner = "OR"
+#   enabled  = true
+
+#   user_labels = {
+#     project_id    = var.project_id
+#     context       = "redis"
+#     instance_id   = var.redis_instance_id
+#     resource_type = "redis_instance"
+#     region        = var.region
+#   }
+
+#   conditions {
+#     display_name = "Cloud Memorystore Redis Instance - Node Role"
+
+#     condition_threshold {
+#       filter = "resource.type = \"redis_instance\" AND resource.labels.instance_id = \"projects/${var.project_id}/locations/us-central1/instances/${var.redis_instance_id}\" AND metric.type = \"redis.googleapis.com/replication/role\""
+
+
+#       duration        = "0s"
+#       comparison      = "COMPARISON_GT"
+#       threshold_value = 0
+
+#       trigger {
+#         count = 1
+#       }
+
+#       aggregations {
+#         alignment_period   = "300s"
+#         per_series_aligner = "ALIGN_STDDEV"
+#       }
+#     }
+#   }
+
+#   alert_strategy {
+#     auto_close = "604800s"
+#   }
+
+#   notification_channels = [google_monitoring_notification_channel.email.id]
+# }
+
+# resource "google_logging_metric" "redis_connection_lost" {
+#   name        = "redis_replica_connection_lost"
+#   description = "Counts how often Redis replica loses connection to the master"
+
+#   filter = <<EOT
+# resource.type="redis_instance"
+# jsonPayload.message:"Connection with master lost"
+# EOT
+
+#   metric_descriptor {
+#     metric_kind = "DELTA"
+#     value_type  = "INT64"
+#     unit        = "1"
+#   }
+
+#   label_extractors = {}
+# }
 # resource "google_monitoring_alert_policy" "redis_connection_lost_alert" {
 #   display_name = "${var.environment} - Redis Replica Lost Master Connection"
 
@@ -425,6 +500,35 @@ resource "google_monitoring_alert_policy" "redis_failover" {
 #         count = 1
 #       }
 #     }
+#   }
+#   notification_channels = [google_monitoring_notification_channel.email.id]
+# }
+# resource "google_monitoring_alert_policy" "evictions" {
+#   display_name = "${var.environment} - Redis Evictions > 0"
+#   combiner     = "OR"
+#   conditions {
+#     display_name = "Evictions > 0"
+#     condition_threshold {
+#       filter          = "metric.type=\"redis.googleapis.com/stats/evicted_keys\" resource.type=\"redis_instance\" resource.label.\"instance_id\"=\"${var.redis_instance_id}\""
+#       comparison      = "COMPARISON_GT"
+#       threshold_value = 0
+#       duration        = "60s"
+#       trigger {
+#         count = 1
+#       }
+#       aggregations {
+#         alignment_period     = "60s"
+#         per_series_aligner   = "ALIGN_RATE"
+#         cross_series_reducer = "REDUCE_NONE"g
+#       }
+#     }
+#   }
+#   user_labels = {
+#     project_id    = var.project_id
+#     context       = "redis"
+#     instance_id   = var.redis_instance_id
+#     resource_type = "redis_instance"
+#     region        = var.region
 #   }
 #   notification_channels = [google_monitoring_notification_channel.email.id]
 # }
